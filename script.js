8090a2fda2ed3672762e6c2f09463f78212ca629
Alex Rieser <rieser.alex@me.com>
2024-10-12 16:34:10 +0200

    website finished except for content

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function getRandomNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function typeText(id, text, speed) {
+function typeText(id, text, speed, callback) {
   let outputElement = document.getElementById(id);
   let index = 0;
 
@@ -10,19 +10,21 @@ function typeText(id, text, speed) {
     if (index < text.length) {
       outputElement.innerHTML += text[index];
       index++;
-      setTimeout(type, getRandomNumber(speed - 50, speed + 20)); // Adjust the speed here
+      setTimeout(type, getRandomNumber(speed - 50, speed + 20));
+    } else if (callback) {
+      callback();
     }
   }
 
-  type(); // Start the typing process
+  type();
 }
 
 function typeAllTexts() {
-  typeText("output", 'ALEXANDER RIESER',140);
-  typeText("1", 'About Me',100);
-  typeText("2", 'Projects',100);
-  typeText("3", 'Blog',100);
-  typeText("4", 'Contact',100);
+  const name = "ALEXANDER RIESER";
+  
+  typeText("name", name, 100, function() {
+    document.getElementById("name").classList.add("pulsate");
+  });
 }
 
 window.onload = typeAllTexts;
