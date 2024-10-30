7026797dcbc1da10bd06e9b72f1db378924489e9
Alex Rieser <rieser.alex@me.com>
2024-10-30 22:06:16 +0100

    sent out 5 test emails, since I have a lot to do this week

--- script.js
+++ script.js
@@ -31,3 +31,4 @@ window.onload = typeAllTexts;
 
 //added mails to the list, and adde website to break room
 //created emails still need to test website availability tomorrow
+//sent out 5 test emails, since I have a lot to do this week
