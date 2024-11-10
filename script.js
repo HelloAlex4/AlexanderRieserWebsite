e400ad4e25762c9cefaa741c1476fcaca6d30bb8
Alex Rieser <rieser.alex@me.com>
2024-11-10 21:00:48 +0100

    d

--- script.js
+++ script.js
@@ -28,11 +28,3 @@ function typeAllTexts() {
 }
 
 window.onload = typeAllTexts;
-
-//added mails to the list, and adde website to break room
-//created emails still need to test website availability tomorrow
-//sent out 5 test emails, since I have a lot to do this week
-//learned wayyyy too much french today. and no one has replied to my mails yet.
-//idk did nothing today.
-//first customer
-//i want that green thing
